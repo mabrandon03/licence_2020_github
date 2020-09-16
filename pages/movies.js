be8f319158af863c8ddf25b01a21ea9a4a139172
Loïc Delestra <git@delestra.com>
2020-09-16 10:31:25 +0200

    Add movies page

---
+++ pages/movies.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Movies = () => {
+  return (
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=, initial-scale=1.0"/>
+        <title>Document</title>
+      </head>
+      <body className="text-center">
+        <h2 className="text-2xl text_blue-400">Best movies</h2>
+      </body>
+    </html>
+  )
+}
+
+
+export default Movies;
